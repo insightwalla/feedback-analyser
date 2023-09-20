1b3406e91db6efb4e698a8f5fe5b08624467955f
insightwalla <127964633+insightwalla@users.noreply.github.com>
2023-09-20 13:57:30 +0100

    trying to solve the issue

--- google_big_query.py
+++ google_big_query.py
@@ -77,10 +77,14 @@ class TransformationGoogleBigQuery:
         #df['Minute_Start_Check'] = df['Minute_Start_Check'].astype(str).str.zfill(2)
         #df['Hour_Close_Check'] = df['Hour_Close_Check'].astype(str).str.zfill(2)
         #df['Minute_Close_Check'] = df['Minute_Close_Check'].astype(str).str.zfill(2)
-        df.loc[:, 'Hour_Start_Check'] = df['Hour_Start_Check'].astype(str).str.zfill(2)  
-        df.loc[:, 'Minute_Start_Check'] = df['Minute_Start_Check'].astype(str).str.zfill(2)
-        df.loc[:, 'Hour_Close_Check'] = df['Hour_Close_Check'].astype(str).str.zfill(2)
-        df.loc[:, 'Minute_Close_Check'] = df['Minute_Close_Check'].astype(str).str.zfill(2)
+        # df.loc[:, 'Hour_Start_Check'] = df['Hour_Start_Check'].astype(str).str.zfill(2)  
+        # df.loc[:, 'Minute_Start_Check'] = df['Minute_Start_Check'].astype(str).str.zfill(2)
+        # df.loc[:, 'Hour_Close_Check'] = df['Hour_Close_Check'].astype(str).str.zfill(2)
+        # df.loc[:, 'Minute_Close_Check'] = df['Minute_Close_Check'].astype(str).str.zfill(2)
+        df.loc[:, 'Hour_Start_Check'] = df['Hour_Start_Check'].astype(str).str.zfill(2).copy()
+        df.loc[:, 'Minute_Start_Check'] = df['Minute_Start_Check'].astype(str).str.zfill(2).copy()
+        df.loc[:, 'Hour_Close_Check'] = df['Hour_Close_Check'].astype(str).str.zfill(2).copy()
+        df.loc[:, 'Minute_Close_Check'] = df['Minute_Close_Check'].astype(str).str.zfill(2).copy()
 
         # modify hour start check if < 7 
         df.loc[df['Hour_Start_Check'].astype(int) < 4, 'Hour_Start_Check'] = (df['Hour_Start_Check'].astype(int) + 24).astype(str).str.zfill(2)
